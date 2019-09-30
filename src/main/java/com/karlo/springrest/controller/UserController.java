058f93dc3cb6931cdc0b443ad301da18f03ea07a
KarloFab <fabijanickarlo1@gmail.com>
2019-09-30 20:39:40 +0200

    Added thymeleaf templates

---
+++ src/main/java/com/karlo/springrest/controller/UserController.java
@@ -0,0 +1,12 @@
+package com.karlo.springrest.controller;
+
+import com.karlo.springrest.service.ApiService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+
+@Slf4j
+@Controller
+public class UserController {
+
+    private ApiService apiService;
+}
