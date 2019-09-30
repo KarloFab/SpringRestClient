43b2bca0030a267f8d37bd5cb5121ff1846d9179
KarloFab <fabijanickarlo1@gmail.com>
2019-09-30 21:23:24 +0200

    Added users controller and test for controller

--- src/main/java/com/karlo/springrest/controller/UserController.java
+++ src/main/java/com/karlo/springrest/controller/UserController.java
@@ -3,10 +3,38 @@ package com.karlo.springrest.controller;
 import com.karlo.springrest.service.ApiService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.server.ServerWebExchange;
 
 @Slf4j
 @Controller
 public class UserController {
 
     private ApiService apiService;
+
+    public UserController(ApiService apiService) {
+        this.apiService = apiService;
+    }
+
+    @GetMapping({"","/","/index"})
+    public String index(){
+        return "index";
+    }
+
+    @PostMapping("/users")
+    public String formPost(Model model, ServerWebExchange serverWebExchange){
+        MultiValueMap<String, String> map = serverWebExchange.getFormData().block();
+
+        int limit = Integer.parseInt(map.get("limit").get(0));
+        if(limit == 0){
+            limit = 10;
+        }
+
+        model.addAttribute("users", apiService.getUsers(limit));
+
+        return "userlist";
+    }
 }
